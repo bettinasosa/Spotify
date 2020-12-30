4632e97e9bbe7c73993c3b6cf563748d261d1b97
Bettina Sosa <40121440+bettinasosa@users.noreply.github.com>
2020-12-30 20:54:02 +0100

    secrets update

--- Client/main.py
+++ Client/main.py
@@ -4,8 +4,12 @@
 
 from spotify_client import *
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+client_id = os.getenv("SPOTIPY_CLIENT_ID")
+client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 
 spotify = SpotifyAPI(client_id, client_secret)
 spotify.search({"track": "Time"}, search_type="track")
