c451ddca146c0a212e50efed2ee238125e6d9584
Bettina Sosa <40121440+bettinasosa@users.noreply.github.com>
2021-01-05 15:39:34 +0100

    updates

--- test.py
+++ test.py
@@ -1,12 +1,43 @@
+from pprint import pprint
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 
 load_dotenv()
 
+client_credentials_manager = SpotifyClientCredentials()
+
 scope = 'playlist-read-private'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+search_str = 'Muse'
+result = sp.search(search_str)
+pprint(result)
 
 results = sp.current_user_playlists(limit=50)
+playList = []
 for i, item in enumerate(results['items']):
-    print("%d %s" % (i, item['name']))
+    playList.append(
+        dict(name=item['name'], id=item['id']))
+
+
+for playlist in playList:
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    pl_id = playlist['id']
+    offset = 0
+    trackList = []
+    while True:
+        response = sp.playlist_items(pl_id,
+                                     offset=offset,
+                                     fields='items.track.id,items.track.name,total',
+                                     additional_types=['track'])
+
+        for i, item in enumerate(response['items']):
+            trackList.append(
+                dict(track=item['track']))
+        print(trackList)
+
+        if len(response['items']) == 0:
+            break
+
+    # pprint(trackList)
+    offset = offset + len(response['items'])
