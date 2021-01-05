559fc0a7f722de7929f2d15e2da6d1da9397f1b6
Bettina Sosa <40121440+bettinasosa@users.noreply.github.com>
2021-01-05 17:03:24 +0100

    Update gettin_playlist_data.py

--- gettin_playlist_data.py
+++ gettin_playlist_data.py
@@ -17,24 +17,26 @@ for i, item in enumerate(results['items']):
     playList.append(
         dict(name=item['name'], id=item['id']))
 
-
+trackList = []
+trackLists = []
 for playlist in playList:
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     pl_id = playlist['id']
     offset = 0
-    trackList = []
     response = sp.playlist_items(pl_id,
                                  offset=offset,
                                  fields='items.track.id,items.track.name',
                                  additional_types=['track'])
-
-trackList = []
-for i in response['items']:
-    tracks = i['track']['name']
-    track_id = i['track']['id']
+    for i, item in enumerate(response['items']):
+        trackLists.append(
+            dict(track=item['track']))
+    pprint(trackLists)
+for song in trackLists:
+    tracks = song['track']['name']
+    track_id = song['track']['id']
     trackList.append(
         dict(name=tracks, id=track_id))
-    # print(trackList)
+# pprint(trackList)
 
 tracks_with_features = []
 for i in trackList:
@@ -54,7 +56,7 @@ for i in trackList:
         liveness=f['liveness'],
         valence=f['valence']
     ))
-print(tracks_with_features[0])
+# print(tracks_with_features[0])
 
 csv_columns = ['name', 'id', 'danceability',
                'energy', 'loudness', 'speechiness', 'acousticness', 'tempo', 'liveness', 'valence']
